0e029336042459675ac5b88ec53a1c95053392e0
Ryan LaBouve <imryanlabouve@gmail.com>
2016-03-02 23:57:34 -0600

    Spike to port basic header styles

--- blueprints/emberjs-header/index.js
+++ blueprints/emberjs-header/index.js
@@ -1,14 +1,21 @@
 /*jshint node:true*/
 module.exports = {
-  description: ''
+  description: '',
 
   normalizeEntityName: function() {
-    // this prevents an error when the entityName is
-    // not specified (since that doesn't actually matter
-    // to us
+    // noop
   },
 
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
   // }
+
+  afterInstall: function(options) {
+    return this.addBowerPackageToProject('bourbon', '4.2.6').then(() => {
+      return this.addBowerPackageToProject('neat', '1.7.3');
+    });
+  }
 };
